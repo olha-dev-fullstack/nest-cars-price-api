7ffdc5ae8bd63c6a53ae47e947bc29b2f45ec49d
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2024-10-23 13:43:00 +0300

    finish with user module and controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { stringify } from 'querystring';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/updateUser.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('auth')
+export class UserController {
+  constructor(private readonly userServise: UserService) {}
+  @Post('/signup')
+  public async createUser(@Body() body: CreateUserDto) {
+    return this.userServise.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  public async findUser(@Param('id') id:string) {
+    return this.userServise.findOne(parseInt(id));
+  }
+
+  @Get()
+  public async findAllUsers(@Query('email') email: string) {
+    return this.userServise.find(email);
+  }
+
+  @Delete('/:id')
+  public async removeUser(@Param('id') id: string) {
+    return this.userServise.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  public async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userServise.update(parseInt(id), body);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  public async create(email: string, password: string) {
+    const user = this.userRepository.create({ email, password });
+    return this.userRepository.save(user);
+  }
+
+  public async find(email: string) {
+    return this.userRepository.find({ where: { email: email } });
+  }
+  public async findOne(id: number) {
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  public async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    Object.assign(user, attrs);
+    return this.userRepository.save(user);
+  }
+
+  public async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return this.userRepository.remove(user);
+  }
+}
